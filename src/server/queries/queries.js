65d2a39dfd8fe5558398a2232039716613f64db9
Andy Gross <andogro@gmail.com>
2016-03-29 11:17:55 -0600

    added delete route, added join query with authors displayed

--- src/server/queries/queries.js
+++ src/server/queries/queries.js
@@ -9,15 +9,51 @@ function Authors() {
 
 module.exports = {
     getBooks: function(){
-        return Books().select()
+        return knex.from('books').innerJoin('authors_books', 'books.id', 'authors_books.book_id')
+        .innerJoin('authors', 'authors_books.author_id', 'authors.id')
         .then(function(results) {
+            console.log(results);
             return results;
-        }) 
+        });
     },
-    getSingleBook: function(id){
-        return knex.raw("SELECT * FROM restaurants LEFT JOIN reviews ON restaurants.id = reviews.restaurant_id WHERE restaurants.id ="+id)
+    getSingleBook: function(id) {
+      return knex.from('books').innerJoin('authors_books', 'books.id', 'authors_books.book_id')
+        .innerJoin('authors', 'authors_books.author_id', 'authors.id').where('books.id', id)
         .then(function(results) {
+            console.log(results);
             return results;
-        }) 
-    }
-};
+        });
+    },
+    deleteSingleBook: function(id) {
+      return Books().where('id', id)
+            .del().then(function(results) {
+            console.log(results);
+            return results;
+        });
+    },
+    addBook: function(title, genre, description, coverurl) {
+      return Books().insert({
+        title: title,
+        genre: genre,
+        description: description,
+        coverurl: coverurl
+         }).returning('id').then(function (response) {
+      return knex('authors_books').insert({
+        book_id: response[0]
+         }).then (function (firstname, lastname, bio, url) {
+      return Authors().insert({
+         firstname: firstname,
+         lastname: lastname,
+         bio: bio,
+         url: url,
+         }).returning('id').then(function (response) {
+      return knex('authors_books').insert({
+         author_id: response[0],
+                })
+              })
+            })
+          })
+        },
+    };
+
+
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -9,16 +9,58 @@ function Authors() {
   return knex('authors');
 }
 
+// view index
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Bookstore' });
 });
 
+// view all books
 router.get('/books', function(req, res, next) {
   queries.getBooks().then(function(results) {
   res.render('books', {array: results});
-  console.log(results);
-  })
+  });
 });
+
+// view add new books page
+router.get('/books/new', function(req, res, next) {
+  res.render('new');
+  });
+
+// view single book
+router.get('/books/:id', function(req,res,next) {
+  var id = req.params.id;
+  queries.getSingleBook(id).then(function(results) {
+    res.render('singlebook', {array: results});
+  });
+});
+
+// delete single book
+router.post('/books/:id', function(req,res,next) {
+  var id = req.params.id;
+  queries.deleteSingleBook(id).then(function() {
+    res.redirect('/books');
+  });
+});
+
+
+router.post('/books/new', function(req, res, next) {
+  var title = req.body.title;
+  var genre = req.body.genre;
+  var desc = req.body.desc;
+  var coverurl = req.body.coverurl;
+  var firstname = req.body.firstname;
+  var lastname = req.body.lastname;
+  var bio = req.body.bio;
+  var portraiturl = req.body.portraiturl;
+  queries.addBook(title, genre, desc, coverurl, firstname, lastname, bio, portraiturl).then(function(results) {
+  res.redirect('/books');
+  });
+});
+
+
+
+
+
 
 
 module.exports = router;
